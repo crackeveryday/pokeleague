9274d260887813a5c9d2322ece7110bc4092ac91
crackeveryday <uts1172017@gmail.com>
2025-02-05 00:24:21 +0900

    rm unnecessary files

--- src/backend/data/prepare_ja_en_dict.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-with open('en_ja_dict.pkl', 'rb') as f:
-    en_ja_dict = pickle.load(f)
-
-ja_en_dict = {v: k for k,v in en_ja_dict.items()}
-print(ja_en_dict)
-with open('ja_en_dict.pkl', 'wb') as f:
-    pickle.dump(ja_en_dict, f)
