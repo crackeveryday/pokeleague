6ca44a92d3fa3ceff379b00417c48bd18e47e955
crackeveryday <uts1172017@gmail.com>
2025-02-06 12:03:40 +0900

    技名を日本語化

--- src/backend/services/quiz_service.py
+++ src/backend/services/quiz_service.py
@@ -1,26 +1,24 @@
 import random
 from models.game import Game
 from services.pokemon_service import PokemonService
-from services.name_converter import PokemonNameConverter
+from backend.services.pokemon_name_converter import PokemonNameConverter
+from backend.services.move_name_converter import MoveNameConverter
 
 class QuizService:
     def __init__(self):
         self.pokemon_service = PokemonService()
         self.name_converter = PokemonNameConverter()
+        self.move_converter = MoveNameConverter()
         self.active_games = {}
-        self.available_moves = [
-            'surf',
-            'thunderbolt',
-            'flamethrower',
-            'psychic',
-            'earthquake'
-        ]
+        self.available_moves = list(self.move_converter.ja_to_en.keys())
 
     def create_new_game(self):
         """新しいゲームを作成"""
-        move_name = random.choice(self.available_moves)
+        move_name_ja = random.choice(self.available_moves)
+        move_name_en = self.move_converter.ja_to_en[move_name_ja][0]
         # 技を覚えるポケモンのリストを取得（英語名）
-        pokemon_list = self.pokemon_service.get_pokemon_by_move(move_name)
+        pokemon_list = self.pokemon_service.get_pokemon_by_move(move_name_en)
+        self.available_moves.remove(move_name_ja)
         # 日本語名に変換
         correct_answers = [
             self.name_converter.get_japanese_name(name) 
@@ -28,7 +26,7 @@ class QuizService:
             if self.name_converter.get_japanese_name(name)
         ]
         
-        game = Game(move_name, correct_answers)
+        game = Game(move_name_en, correct_answers)
         self.active_games[game.id] = game
         return game
 
