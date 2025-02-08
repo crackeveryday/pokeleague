7280e0a9b89575eab4d7dbf96b358b443999e755
crackeveryday <uts1172017@gmail.com>
2025-02-08 13:49:31 +0900

    ポケモンの名前辞書の更新

--- src/backend/data_scripts/prepare_ja_en_dict.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-with open('pokemon_names_en_ja.pkl', 'rb') as f:
-    en_ja_dict = pickle.load(f)
-
-ja_en_dict = {v: k for k,v in en_ja_dict.items()}
-print(ja_en_dict)
-with open('pokemon_names_ja_en.pkl', 'wb') as f:
-    pickle.dump(ja_en_dict, f)
