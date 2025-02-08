daa6d6b91b04086b23095a0bf3d649d97e07f732
crackeveryday <uts1172017@gmail.com>
2025-02-08 12:19:07 +0900

    ポケモン名の日英辞書も同じスクリプトで作成するよう変更

--- src/backend/data_scripts/prepare_all_poke_name.py
+++ src/backend/data_scripts/prepare_all_poke_name.py
@@ -18,6 +18,9 @@ for pokemon in all_pokemon:
     url = base_url + pokemon
     response = requests.get(url).json()
     jp_name = response['names'][0]['name']
+    # pokemon-speciesエンドポイントと他とで、ポケモン名にフォルムが入っているかの違いがある
+    # ポケモンの英語名をデフォルトのフォルム名に統一する
+    # pokemon-speciesからしか日本語名が取れないためこうしている
     en_default_name = response['varieties'][0]['pokemon']['name']
     en_ja_dict[en_default_name] = jp_name
     time.sleep(0.1)
@@ -27,3 +30,8 @@ for pokemon in all_pokemon:
 print(en_ja_dict)
 with open('pokemon_names_en_ja.pkl', 'wb') as f:
     pickle.dump(en_ja_dict, f)
+
+ja_en_dict = {v: k for k,v in en_ja_dict.items()}
+print(ja_en_dict)
+with open('pokemon_names_ja_en.pkl', 'wb') as f:
+    pickle.dump(ja_en_dict, f)
